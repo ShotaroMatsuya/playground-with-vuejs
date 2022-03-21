b36f69efdfda02a272302e8dec90acc0f4917e4f
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-03-21 20:58:13 +0900

    finished assignment

---
+++ app/src/components/ActiveUser.vue
@@ -0,0 +1,25 @@
+<template>
+  <li>
+    <h2>username: {{ this.name }}</h2>
+    <h3>age: {{ this.age }}</h3>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
---
+++ app/src/components/UserData.vue
@@ -0,0 +1,40 @@
+<template>
+  <section>
+    <h2>新規登録</h2>
+    <form @submit.prevent="submitUser">
+      <div>
+        <label for="name">名前：</label>
+        <input id="name" type="name" v-model="name" />
+      </div>
+      <div>
+        <label for="age">年齢：</label>
+        <input id="age" type="age" v-model="age" />
+      </div>
+      <button>登録</button>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  emits: ['insert-user'],
+  data() {
+    return {
+      name: '',
+      age: '',
+    };
+  },
+  methods: {
+    submitUser() {
+      const userInfo = {
+        id: new Date().toISOString(),
+        name: this.name,
+        age: this.age,
+      };
+      this.$emit('insert-user', userInfo);
+      this.name = '';
+      this.age = '';
+    },
+  },
+};
+</script>
