2dca4ceba5bf7715e43c67c9733b8146db2a2b52
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-03-22 21:27:30 +0900

    learning slots

--- app/src/components/BaseBadge.vue
+++ app/src/components/BaseBadge.vue
@@ -2,7 +2,7 @@
   <span class="badge" :class="classes">{{ caption }}</span>
 </template>
 
-<script>
+<script scoped>
 export default {
   props: ['type', 'caption'],
   computed: {
@@ -16,7 +16,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .badge {
   display: inline-block;
   padding: 0.5rem 1rem;
