3e84e7c03f103c4bcb42b25744e054c07fc3a0dc
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-03-26 16:25:47 +0900

    added new pj

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,55 +1,29 @@
 <template>
-  <div>
-    <the-header></the-header>
-    <!-- <TheHeader /> -->
-    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
-    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
-    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
-    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals> -->
-    <keep-alive>
-      <component :is="selectedComponent"></component>
-    </keep-alive>
-  </div>
+  <the-header title="RememberMe"></the-header>
+  <the-resources></the-resources>
 </template>
 
 <script>
-import TheHeader from './components/layout/TheHeader.vue';
-// import BadgeList from './components/BadgeList.vue';
-// import UserInfo from './components/UserInfo.vue';
-// import CourseGoals from './components/CourseGoals.vue';
-import ActiveGoals from './components/ActiveGoals.vue';
-import ManageGoals from './components/ManageGoals.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
+import TheResources from './components/learning-resources/TheResources.vue';
 
 export default {
   components: {
     TheHeader,
-    // BadgeList,
-    // UserInfo,
-    // CourseGoals,
-    ActiveGoals,
-    ManageGoals,
-  },
-  data() {
-    return {
-      selectedComponent: 'active-goals',
-      activeUser: {
-        name: 'Maximilian Schwarzmüller',
-        description: 'Site owner and admin',
-        role: 'admin',
-      },
-    };
-  },
-  methods: {
-    setSelectedComponent(cmp) {
-      this.selectedComponent = cmp;
-    },
+    TheResources,
   },
 };
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
 html {
-  font-family: sans-serif;
+  font-family: 'Roboto', sans-serif;
 }
 
 body {
