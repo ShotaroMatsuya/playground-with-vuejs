3d86d878d8c7301cc4351816f8d00850b7357bd0
ShotaroMatsuya <matsuya@h01.itscom.net>
2022-03-30 02:31:16 +0900

    small fixed

--- app/src/App.vue
+++ app/src/App.vue
@@ -9,77 +9,83 @@
 <script>
 export default {
   data() {
-    return { animatedBlock: false, dialogIsVisible: false, paraIsVisible: false, usersAreVisible: false,enterInterval: null , leaveInterval: null };
+    return {
+      animatedBlock: false,
+      dialogIsVisible: false,
+      paraIsVisible: false,
+      usersAreVisible: false,
+      enterInterval: null,
+      leaveInterval: null,
+    };
   },
   methods: {
-    enterCancelled(el){
+    enterCancelled(el) {
       console.log(el);
       clearInterval(this.enterInterval);
     },
-    leaveCancelled(el){
+    leaveCancelled(el) {
       console.log(el);
       clearInterval(this.leaveInterval);
     },
-    beforeEnter(el){
+    beforeEnter(el) {
       console.log('beforeEnter');
       console.log(el);
       el.style.opacity = 0;
     },
-    enter(el,done){
+    enter(el, done) {
       console.log('enter');
       console.log(el);
       let round = 1;
-      this.enterInterval = setInterval(()=>{
+      this.enterInterval = setInterval(() => {
         el.style.opacity = round * 0.01;
-        round ++;
-        if(round > 100){
+        round++;
+        if (round > 100) {
           clearInterval(this.enterInterval);
           done();
         }
-      },20);
+      }, 20);
     },
-    afterEnter(el){
+    afterEnter(el) {
       console.log('afterEnter');
       console.log(el);
     },
-    beforeLeave(el){
+    beforeLeave(el) {
       console.log('beforeLeave');
       console.log(el);
     },
-    beforeLeave(el){
+    beforeLeave(el) {
       console.log('beforeLeave');
       console.log(el);
       el.style.opacity = 1;
     },
-    leave(el,done){
+    leave(el, done) {
       console.log('leave');
       console.log(el);
       let round = 1;
-      this.leaveInterval = setInterval(()=>{
+      this.leaveInterval = setInterval(() => {
         el.style.opacity = 1 - round * 0.01;
-        round ++;
-        if(round > 100){
+        round++;
+        if (round > 100) {
           clearInterval(this.leaveInterval);
           done();
         }
-      },20);
-
+      }, 20);
     },
-    afterLeave(el){
+    afterLeave(el) {
       console.log('afterLeave');
       console.log(el);
     },
-    showUsers(){
+    showUsers() {
       this.usersAreVisible = true;
     },
-    hideUsers(){
+    hideUsers() {
       this.usersAreVisible = false;
     },
     animateBlock() {
       this.animatedBlock = true;
     },
-    toggleParagraph(){
-      this.paraIsVisible =  !this.paraIsVisible;
+    toggleParagraph() {
+      this.paraIsVisible = !this.paraIsVisible;
     },
     showDialog() {
       this.dialogIsVisible = true;
@@ -134,49 +140,51 @@ button:active {
   border-radius: 12px;
 }
 .animate {
-  transform: translateX(-150px);
+  /* transform: translateX(-150px); */
   animation: slide-fade 2s ease-out;
 }
 
-.modal-enter-from{
-
+.modal-enter-from {
 }
-.modal-enter-active{
+.modal-enter-active {
   animation: modal 0.3s ease-out;
 }
-.modal-enter-to{}
+.modal-enter-to {
+}
 
 .fade-button-enter-from,
-.fade-button-leave-from{
+.fade-button-leave-from {
   opacity: 0;
 }
-.fade-button-enter-active{
+.fade-button-enter-active {
   transition: opacity 0.3s ease-out;
 }
-.fade-button-leave-active{
+.fade-button-leave-active {
   transition: opacity 0.3s ease-in;
 }
 .fade-button-enter-from,
-.fade-button-leave-to{
+.fade-button-leave-to {
   opacity: 1;
 }
 
-.route-enter-from{}
-.route-enter-active{
+.route-enter-from {
+}
+.route-enter-active {
   animation: slide-scale 0.4s ease-out;
 }
-.route-enter-to{}
+.route-enter-to {
+}
 
-.route-leave-active{
+.route-leave-active {
   animation: slide-scale 0.4s ease-in;
 }
 
-@keyframes modal{
+@keyframes modal {
   from {
     opacity: 0;
     transform: translateY(-50px) scale(0.9);
   }
-  to{
+  to {
     opacity: 1;
     transform: translateY(0) scale(1);
   }
