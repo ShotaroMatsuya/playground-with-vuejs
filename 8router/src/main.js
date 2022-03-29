1362f52f6acdcf55456b9847691a995b4b4e9313
ShotaroMatsuya <matsuya@h01.itscom.net>
2022-03-29 11:38:29 +0900

    animation basic css transition

--- 8router/src/main.js
+++ 8router/src/main.js
@@ -1,10 +1,10 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
-import BaseModal from './components/BaseModal.vue';
+import router from './router.js';
 
 const app = createApp(App);
 
-app.component('base-modal', BaseModal);
+app.use(router);
 
 app.mount('#app');
