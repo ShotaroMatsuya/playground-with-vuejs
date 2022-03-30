68b9d8ecc1b32e3f9eea3b6577eec58a674b2662
ShotaroMatsuya <matsuya@h01.itscom.net>
2022-03-30 20:11:30 +0900

    namespacing mudules

--- app/src/main.js
+++ app/src/main.js
@@ -5,6 +5,7 @@ import {createStore} from 'vuex';
 import App from './App.vue';
 
 const counterModule ={
+    namespaced: true,
     state(){
         return {
             counter: 0
@@ -25,11 +26,16 @@ const counterModule ={
             },2000);
         },
         increase(context,payload){
+            console.log(context);
             context.commit('increase',payload);
         },
+        login(){}
 
     },
     getters: {
+        testAuth(state, getters,rootState,rootGetters){
+            return state.isLoggedIn;
+        },
         finalCounter(state){
             return state.counter * 3;
             },
