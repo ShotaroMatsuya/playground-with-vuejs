eb6c97fb0fbed4f19e40e170f401969ace4a9eb4
ShotaroMatsuya <matsuya@h01.itscom.net>
2022-04-06 08:54:11 +0900

    added routes

--- app/src/router.js
+++ app/src/router.js
@@ -1,17 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router';
-
-import ProductsList from './pages/ProductsList.vue';
-import UserCart from './pages/UserCart.vue';
-import ShopAdmin from './pages/ShopAdmin.vue';
+import { createRouter } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', redirect: '/products' },
-    { path: '/products', component: ProductsList },
-    { path: '/cart', component: UserCart },
-    { path: '/admin', component: ShopAdmin },
-  ]
+    { path: '/', redirect: 'coaches' },
+    { path: '/coaches', component: null },
+    {
+      path: '/coaches/:id',
+      component: null,
+      children: [
+        { path: 'contact', component: null }, // /coaches/c1/contact
+      ],
+    },
+    { path: '/register', component: null },
+    { path: '/requests', component: null },
+    { path: '/:notFound(.*)', component: null },
+  ],
 });
-
-export default router;
