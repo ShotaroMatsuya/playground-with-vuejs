f3b2a1e4e9f4be43b5c8b65017751a47e859c7c0
ShotaroMatsuya <matsuya@h01.itscom.net>
2022-04-12 00:00:57 +0900

    fetching coach data

--- app/src/store/modules/coaches/actions.js
+++ app/src/store/modules/coaches/actions.js
@@ -17,7 +17,7 @@ export default {
       }
     );
 
-    const responseData = await response.json();
+    // const responseData = await response.json();
 
     if (!response.ok) {
       // error...
@@ -27,5 +27,29 @@ export default {
       ...coachData,
       id: userId,
     });
+  },
+  async loadCoaches(context) {
+    const response = await fetch(
+      `https://vue-http-demo-fa7db-default-rtdb.asia-southeast1.firebasedatabase.app/coaches.json`
+    );
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      // ...
+    }
+    const coaches = [];
+
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas,
+      };
+      coaches.push(coach);
+    }
+    context.commit('setCoaches', coaches);
   },
 };
