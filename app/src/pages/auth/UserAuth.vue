c27dbfafa8e6a4aea08fe84c13262bbf8dede12a
ShotaroMatsuya <matsuya@h01.itscom.net>
2022-04-12 16:31:27 +0900

    implement login functionality

--- app/src/pages/auth/UserAuth.vue
+++ app/src/pages/auth/UserAuth.vue
@@ -60,15 +60,17 @@ export default {
         return ;
       }
       this.isLoading = true;
+
+      const actionPayload = {
+        email: this.email,
+        password: this.password
+      }
       // send http request...
       try{
         if(this.mode === 'login'){
-          // ...
+          await this.$store.dispatch('login',actionPayload);
         }else{
-          await this.$store.dispatch('signup',{
-            email: this.email,
-            password: this.password
-          });
+          await this.$store.dispatch('signup',actionPayload);
         }
       }catch(err){
         this.error = err.message || 'Failed to authenticate, try later. Check your login data.';
