7b68640265873ede399f1579b1d9168ca609feef
ShotaroMatsuya <matsuya@h01.itscom.net>
2022-04-13 05:09:35 +0900

    ataching the token

--- app/src/store/modules/coaches/actions.js
+++ app/src/store/modules/coaches/actions.js
@@ -8,9 +8,10 @@ export default {
       hourlyRate: data.rate,
       areas: data.areas,
     };
+    const token = context.rootGetters.token;
 
     const response = await fetch(
-      `https://vue-http-demo-fa7db-default-rtdb.asia-southeast1.firebasedatabase.app/coaches/${userId}.json`,
+      `https://vue-http-demo-fa7db-default-rtdb.asia-southeast1.firebasedatabase.app/coaches/${userId}.json?auth=` + token,
       {
         method: 'PUT',
         body: JSON.stringify(coachData),
