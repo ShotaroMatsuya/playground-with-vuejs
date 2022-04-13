32efabf0d41d9352bc061e282b4191b9c87631be
ShotaroMatsuya <matsuya@h01.itscom.net>
2022-04-13 16:08:10 +0900

    replace computed properties with the computed func

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,70 +1,65 @@
 <template>
-  <h2>My Course Goal</h2>
-  <!-- Task 1: Output your main course goal with help of the composition API -->
-  <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
-  <!-- <p v-if="isShow">{{myGoal}}</p> -->
-  <!-- <p v-if="obj.isShow">{{obj.myGoal}}</p> -->
-  <!-- <p v-if="reactiveObj.isShow">{{reactiveObj.myGoal}}</p> -->
-  <p v-if="isShow">{{myGoal}}</p>
-  <h3>OUTPUT COURSE GOAL</h3>
-  <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
-  <button @click="toggleGoal">Toggle Goal</button>
-  <!-- Task 3: Manage data in three ways -->
-  <!-- => Separate refs -->
-  <!-- => Ref Object -->
-  <!-- => Reactive Object -->
-  <!-- Task 4: Also solve the assignment with the Options API -->
+  <section class="container">
+    <h2>{{ userName }}</h2>
+    <h3>{{ age }}</h3>
+    <button @click="setAge">Change Age</button>
+    <div>
+      <input type="text" placeholder="First Name" @input="setFirstName" />
+      <input type="text" placeholder="Last Name" @input="setLastName" />
+    </div>
+  </section>
 </template>
 
 <script>
-// import { reactive} from 'vue';
+import {ref,computed} from 'vue';
+
 export default {
 
-  data(){
-    return{
-      myGoal:'This is not my ideal.',
-      isShow: true
-    }
-  },
-  methods: {
-    toggleGoal(){
-      this.isShow = !this.isShow;
-    }
-  }
-  
-  // setup(){
+  setup(){
+    // const uName = ref('showtarwo');
+    const firstName = ref('');
+    const lastName = ref('');
+    const uAge = ref(18);
 
-  //   // const myGoal = ref('I will complete this course.');
-  //   // const isShow = ref(true);
-  //   // const obj = ref({
-  //   //   myGoal : 'I will complete this course.',
-  //   //   isShow: true
-  //   // })
-  //   const reactiveObj = reactive({
-  //     myGoal : 'I will complete this course.',
-  //     isShow: true    
-  //   });
-  //   function toggleGoal(){
-  //     // isShow.value = !isShow.value;
-  //     // obj.value.isShow = !obj.value.isShow;
-  //     reactiveObj.isShow = !reactiveObj.isShow;
-  //   }
-  //   // return {
-  //   //   myGoal, isShow, toggleGoal
-  //   // }
-  //   // return {obj, toggleGoal}
-  //   return {reactiveObj, toggleGoal}
-  // }
-  
-}
+    const uName = computed(function(){
+      return firstName.value + ' ' + lastName.value;
+    });
+
+    function setNewData(){
+      uAge.value = 20;
+    }
+    function setFirstName(event){
+      firstName.value = event.target.value;
+    }
+    function setLastName(event){
+      lastName.value = event.target.value;
+    }
+    return {userName: uName, age: uAge, setAge: setNewData, setFirstName,setLastName};
+  },
+
+
+};
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+
 html {
   font-family: sans-serif;
 }
+
 body {
-  margin: 3rem;
+  margin: 0;
+}
+
+.container {
+  margin: 3rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
   text-align: center;
 }
 </style>
---
+++ assignment6/src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <h2>My Course Goal</h2>
+  <!-- Task 1: Output your main course goal with help of the composition API -->
+  <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
+  <!-- <p v-if="isShow">{{myGoal}}</p> -->
+  <!-- <p v-if="obj.isShow">{{obj.myGoal}}</p> -->
+  <!-- <p v-if="reactiveObj.isShow">{{reactiveObj.myGoal}}</p> -->
+  <p v-if="isShow">{{myGoal}}</p>
+  <h3>OUTPUT COURSE GOAL</h3>
+  <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
+  <button @click="toggleGoal">Toggle Goal</button>
+  <!-- Task 3: Manage data in three ways -->
+  <!-- => Separate refs -->
+  <!-- => Ref Object -->
+  <!-- => Reactive Object -->
+  <!-- Task 4: Also solve the assignment with the Options API -->
+</template>
+
+<script>
+// import { reactive} from 'vue';
+export default {
+
+  data(){
+    return{
+      myGoal:'This is not my ideal.',
+      isShow: true
+    }
+  },
+  methods: {
+    toggleGoal(){
+      this.isShow = !this.isShow;
+    }
+  }
+  
+  // setup(){
+
+  //   // const myGoal = ref('I will complete this course.');
+  //   // const isShow = ref(true);
+  //   // const obj = ref({
+  //   //   myGoal : 'I will complete this course.',
+  //   //   isShow: true
+  //   // })
+  //   const reactiveObj = reactive({
+  //     myGoal : 'I will complete this course.',
+  //     isShow: true    
+  //   });
+  //   function toggleGoal(){
+  //     // isShow.value = !isShow.value;
+  //     // obj.value.isShow = !obj.value.isShow;
+  //     reactiveObj.isShow = !reactiveObj.isShow;
+  //   }
+  //   // return {
+  //   //   myGoal, isShow, toggleGoal
+  //   // }
+  //   // return {obj, toggleGoal}
+  //   return {reactiveObj, toggleGoal}
+  // }
+  
+}
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+body {
+  margin: 3rem;
+  text-align: center;
+}
+</style>
