b6e6c37316057ec1f56d2f57d715c829b97feef4
ShotaroMatsuya <matsuya@h01.itscom.net>
2022-04-13 09:24:22 +0900

    implement global navigation guards

--- app/src/pages/auth/UserAuth.vue
+++ app/src/pages/auth/UserAuth.vue
@@ -7,88 +7,101 @@
       <base-spinner></base-spinner>
     </base-dialog>
     <base-card>
-  <form @submit.prevent="submitForm">
-    <div class="form-control">
-      <label for="email">E-Mail</label>
-      <input type="email" id="email" v-model.trim="email" />
-    </div>
-    <div class="form-control">
-      <label for="password">Password</label>
-      <input type="password" id="password" v-model.trim="password" />
-    </div>
-    <p v-if="!formIsValid">Please enter a valid email and password (must be at least 6 characters long).</p>
-    <base-button>{{ submitButtonCaption }}</base-button>
-    <base-button type="button" mode="flat" @click="switchAuthMode">{{ switchModeButtonCaption }}</base-button>
-  </form>
+      <form @submit.prevent="submitForm">
+        <div class="form-control">
+          <label for="email">E-Mail</label>
+          <input type="email" id="email" v-model.trim="email" />
+        </div>
+        <div class="form-control">
+          <label for="password">Password</label>
+          <input type="password" id="password" v-model.trim="password" />
+        </div>
+        <p v-if="!formIsValid">
+          Please enter a valid email and password (must be at least 6 characters
+          long).
+        </p>
+        <base-button>{{ submitButtonCaption }}</base-button>
+        <base-button type="button" mode="flat" @click="switchAuthMode">{{
+          switchModeButtonCaption
+        }}</base-button>
+      </form>
     </base-card>
   </div>
 </template>
 
 <script>
 export default {
-  data(){
+  data() {
     return {
-      email:'',
+      email: '',
       password: '',
       formIsValid: true,
       mode: 'login',
       isLoading: false,
-      error: null
+      error: null,
     };
   },
   computed: {
-    submitButtonCaption(){
-      if(this.mode === 'login'){
+    submitButtonCaption() {
+      if (this.mode === 'login') {
         return 'Login';
-      }else{
+      } else {
         return 'Signup';
       }
     },
-    switchModeButtonCaption(){
-      if(this.mode === 'login'){
+    switchModeButtonCaption() {
+      if (this.mode === 'login') {
         return 'Signup instead';
-      }else{
+      } else {
         return 'Login instead';
       }
-    }
+    },
   },
-  methods:{
-    async submitForm(){
+  methods: {
+    async submitForm() {
       this.formIsValid = true;
-      if(this.email === '' || !this.email.includes('@') || this.password.length < 6){
+      if (
+        this.email === '' ||
+        !this.email.includes('@') ||
+        this.password.length < 6
+      ) {
         this.formIsValid = false;
-        return ;
+        return;
       }
       this.isLoading = true;
 
       const actionPayload = {
         email: this.email,
-        password: this.password
-      }
+        password: this.password,
+      };
       // send http request...
-      try{
-        if(this.mode === 'login'){
-          await this.$store.dispatch('login',actionPayload);
-        }else{
-          await this.$store.dispatch('signup',actionPayload);
+      try {
+        if (this.mode === 'login') {
+          await this.$store.dispatch('login', actionPayload);
+        } else {
+          await this.$store.dispatch('signup', actionPayload);
         }
-      }catch(err){
-        this.error = err.message || 'Failed to authenticate, try later. Check your login data.';
+        const redirectUrl = '/' + (this.$route.query.redirect || 'coaches');
+        this.$router.replace(redirectUrl);
+      } catch (err) {
+        this.error =
+          err.message ||
+          'Failed to authenticate, try later. Check your login data.';
       }
       this.isLoading = false;
     },
-    switchAuthMode(){
-      if(this.mode === 'login'){
+    switchAuthMode() {
+      if (this.mode === 'login') {
         this.mode = 'signup';
-      }else{
+      } else {
         this.mode = 'login';
       }
     },
-    handleError(){
+    handleError() {
       this.error = null;
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -122,6 +135,4 @@ textarea:focus {
   background-color: #faf6ff;
   outline: none;
 }
-
-
 </style>
