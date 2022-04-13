920f19af975bcda18d4d6b227971f90914b704c4
ShotaroMatsuya <matsuya@h01.itscom.net>
2022-04-13 15:50:09 +0900

    replace composition with option

--- app/src/App.vue
+++ app/src/App.vue
@@ -4,7 +4,8 @@
   <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
   <!-- <p v-if="isShow">{{myGoal}}</p> -->
   <!-- <p v-if="obj.isShow">{{obj.myGoal}}</p> -->
-  <p v-if="reactiveObj.isShow">{{reactiveObj.myGoal}}</p>
+  <!-- <p v-if="reactiveObj.isShow">{{reactiveObj.myGoal}}</p> -->
+  <p v-if="isShow">{{myGoal}}</p>
   <h3>OUTPUT COURSE GOAL</h3>
   <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
   <button @click="toggleGoal">Toggle Goal</button>
@@ -16,32 +17,44 @@
 </template>
 
 <script>
-import { reactive} from 'vue';
+// import { reactive} from 'vue';
 export default {
-  
-  setup(){
 
-    // const myGoal = ref('I will complete this course.');
-    // const isShow = ref(true);
-    // const obj = ref({
-    //   myGoal : 'I will complete this course.',
-    //   isShow: true
-    // })
-    const reactiveObj = reactive({
-      myGoal : 'I will complete this course.',
-      isShow: true    
-    });
-    function toggleGoal(){
-      // isShow.value = !isShow.value;
-      // obj.value.isShow = !obj.value.isShow;
-      reactiveObj.isShow = !reactiveObj.isShow;
+  data(){
+    return{
+      myGoal:'This is not my ideal.',
+      isShow: true
     }
-    // return {
-    //   myGoal, isShow, toggleGoal
-    // }
-    // return {obj, toggleGoal}
-    return {reactiveObj, toggleGoal}
+  },
+  methods: {
+    toggleGoal(){
+      this.isShow = !this.isShow;
+    }
   }
+  
+  // setup(){
+
+  //   // const myGoal = ref('I will complete this course.');
+  //   // const isShow = ref(true);
+  //   // const obj = ref({
+  //   //   myGoal : 'I will complete this course.',
+  //   //   isShow: true
+  //   // })
+  //   const reactiveObj = reactive({
+  //     myGoal : 'I will complete this course.',
+  //     isShow: true    
+  //   });
+  //   function toggleGoal(){
+  //     // isShow.value = !isShow.value;
+  //     // obj.value.isShow = !obj.value.isShow;
+  //     reactiveObj.isShow = !reactiveObj.isShow;
+  //   }
+  //   // return {
+  //   //   myGoal, isShow, toggleGoal
+  //   // }
+  //   // return {obj, toggleGoal}
+  //   return {reactiveObj, toggleGoal}
+  // }
   
 }
 </script>
