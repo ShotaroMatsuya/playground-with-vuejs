ce9ce3e153081fc289fa276f2e2edd730708537c
ShotaroMatsuya <matsuya@h01.itscom.net>
2022-04-13 15:27:21 +0900

    replace methods with regular func

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,34 +2,40 @@
   <section class="container">
     <h2>{{ userName }}</h2>
     <h3>{{ age }}</h3>
+    <button @click="setAge">Change Age</button>
   </section>
 </template>
 
 <script>
-import {reactive, toRefs,isReactive, isRef} from 'vue';
+import {ref} from 'vue';
 
 export default {
 
   setup(){
-
-    const user = reactive({
-      name: 'Showtarow',
-      age: 18
-    });
-
-    setTimeout(()=>{
-      user.name = 'Show!!!';
-      user.age = 20;
-    },2000);
-
+    const uName = ref('showtarwo');
+    const uAge = ref(18);
+    // const user = reactive({
+    //   name: 'Showtarow',
+    //   age: 18
+    // });
+    function setNewData(){
+      // user.age = 20;
+      uAge.value = 20;
+    }
 
     // reactive obj to refs one 
-    const userRefs = toRefs(user);
-    console.log(isReactive(user));
-    console.log(isRef(userRefs.name));
+    // const userRefs = toRefs(user);
+    // console.log(isReactive(user));
+    // console.log(isRef(userRefs.name));
 
-    return { user: user, userName: userRefs.name, age: userRefs.age };
-  }
+    // return { user: user, userName: userRefs.name, age: userRefs.age };
+    return {userName: uName, age: uAge, setAge: setNewData};
+  },
+  // methods: {
+  //   setNewAge(){
+  //     this.age = 22;
+  //   }
+  // }
 
 };
 </script>
