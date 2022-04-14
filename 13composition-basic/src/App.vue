f0ebd305f0cd46acebe9db21008540d1266ed0f7
ShotaroMatsuya <matsuya@h01.itscom.net>
2022-04-14 17:20:33 +0900

    set starting point

---
+++ 13composition-basic/src/App.vue
@@ -0,0 +1,78 @@
+<template>
+  <section class="container">
+    <user-data :user-name="userName" :first-name="firstName" :last-name="lastName"></user-data>
+    <button @click="setAge">Change Age</button>
+    <div>
+      <input type="text" placeholder="First Name" v-model="firstName" />
+      <input type="text" placeholder="Last Name" ref="lastNameInput" />
+      <button @click="setLastName">Set Last Name</button>
+    </div>
+  </section>
+</template>
+
+<script>
+import {ref,computed, watch, provide} from 'vue';
+import UserData from './components/UserData.vue';
+
+export default {
+  components:{
+    UserData
+  },
+  setup(){
+    // const uName = ref('showtarwo');
+    const firstName = ref('');
+    const lastName = ref('');
+    const lastNameInput = ref(null); // ref prop
+    const uAge = ref(18);
+
+
+    provide('userAge', uAge);
+
+    const uName = computed(function(){
+      return firstName.value + ' ' + lastName.value;
+    });
+
+    watch([uAge, uName],function(newValues, oldValues){
+      console.log('Old age: '+ oldValues[0]);
+      console.log('New age: '+ newValues[0]);
+      console.log('Old name: '+ oldValues[1]);
+      console.log('New name: '+ newValues[1]);
+    })
+    function setNewData(){
+      uAge.value = 20;
+    }
+    function setLastName(){
+      lastName.value = lastNameInput.value.value;
+    }
+
+    return {userName: uName, age: uAge, setAge: setNewData, firstName, lastName , lastNameInput,setLastName };
+  },
+  // provide(){
+  //   return {age: this.age};
+  // }
+
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+.container {
+  margin: 3rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+  text-align: center;
+}
+</style>
